37f520d7a5541bcc0ed2c0c27d8d673249d4474e
amir cohen <amircohen@amirs-MacBook-Pro.local>
2022-03-25 12:05:34 +0300

    UI improvment

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,7 @@ import SideMenu from './components/SideMenu';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCCIn1SHOH58PzU-R1SvT78oxsG6JTWAG4",
-  authDomain: "avaulto.firebaseapp.com",
-  projectId: "avaulto",
-  storageBucket: "avaulto.appspot.com",
-  messagingSenderId: "561694535871",
-  appId: "1:561694535871:web:e393a561d5743e179dd60a",
-  measurementId: "G-V3Y8SGS5J2"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCIn1SHOH58PzU-R1SvT78oxsG6JTWAG4",
+  authDomain: "avaulto.firebaseapp.com",
+  projectId: "avaulto",
+  storageBucket: "avaulto.appspot.com",
+  messagingSenderId: "561694535871",
+  appId: "1:561694535871:web:e393a561d5743e179dd60a",
+  measurementId: "G-V3Y8SGS5J2"
+};
