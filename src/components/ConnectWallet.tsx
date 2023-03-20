a5547e29e93f53c43d913c88b24e5349ef4fa04a
AC-devon <inbox@avaulto.com>
2023-03-20 20:52:49 +0200

    update rpc url

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -18,7 +18,7 @@ import RewardTable from './RewardTable';
 require('@solana/wallet-adapter-react-ui/styles.css');
 const ConnectWallet = () => {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = 'https://mb-avaulto-cc28.mainnet.rpcpool.com/f72a3ed2-f282-4523-95a0-d4acfcd40f4d' //WalletAdapterNetwork.Mainnet;
+    const network = 'https://mb-avaulto-cc28.mainnet.rpcpool.com' //WalletAdapterNetwork.Mainnet;
 
     // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => network, [network]);
